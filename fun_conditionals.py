b09adb45b0fb8cff1697f22e40e687bcc14c304f
Gulmira <miramaxeast@yahoo.com>
2015-11-04 21:32:28 -0800

    class 6

---
+++ fun_conditionals.py
@@ -0,0 +1,8 @@
+def check_for_string (input):
+	if type(input)== str:
+		print input, "It is a string!"
+	else:
+		print input, "It is not a string"
+
+check_for_string (9)
+check_for_string("night")
